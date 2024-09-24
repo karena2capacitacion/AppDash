2275881fba26f0416b42c7e0d1fc3b0cc7235eb5
karena2capacitacion <118851703+karena2capacitacion@users.noreply.github.com>
2024-09-24 14:13:24 -0600

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,6 +1,5 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html, dcc
 import plotly.graph_objs as go
 
 from dash.dependencies import Input, Output
